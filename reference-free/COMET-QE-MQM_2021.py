49e5d587513a394443632ca9820470c387bac8e6
Natalia Khaidanova <n.khaidanova@student.vu.nl>
2023-05-23 19:40:08 +0200

    Create COMET-QE-MQM_2021.py

---
+++ reference-free/COMET-QE-MQM_2021.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from comet import download_model, load_from_checkpoint
+
+
+model_path = download_model('NataliaKhaidanova/wmt21-comet-qe-mqm')
+comet_qe_mqm_2021_model = load_from_checkpoint(model_path)
+
+for file in [r'Data/baby_k.tsv', r'Data/a_beautiful_mind.tsv']:
+    
+    data_dict, comet_qe_mqm_2021_human_scores, comet_qe_mqm_2021_opus_mt_scores = {}, [], []
+    
+    df = pd.read_csv(file, sep='\t') 
+    sources = list(df['source'])
+    human_translations = list(df['human_translation'])
+    opus_mt_translations = list(df['opus_mt_translation'])
+
+    for source, human_translation in zip(sources, human_translations):
+        inputs = [{'src':source,'mt':human_translation}]
+            
+        comet_qe_mqm_2021_score = comet_qe_mqm_2021_model.predict(inputs, batch_size=8, gpus=1)
+        comet_qe_mqm_2021_human_scores.append(f'{comet_qe_mqm_2021_score[0][0]:.3f}')
+        
+    for source, opus_mt_translation in zip(sources, opus_mt_translations):
+        inputs = [{'src':source,'mt':opus_mt_translation}]
+            
+        comet_qe_mqm_2021_score = comet_qe_mqm_2021_model.predict(inputs, batch_size=8, gpus=1)
+        comet_qe_mqm_2021_opus_mt_scores.append(f'{comet_qe_mqm_2021_score[0][0]:.3f}')
+
+
+    data_dict['human_scores'] = comet_qe_mqm_2021_human_scores    
+    data_dict['opus_mt_scores'] = comet_qe_mqm_2021_opus_mt_scores    
+
+    comet_qe_mqm_2021_data = pd.DataFrame(data_dict)
+    comet_qe_mqm_2021_data.to_csv(f'Data/{file[5:-4]}_COMET-QE-MQM_2021.tsv', sep='\t', index=False) 
