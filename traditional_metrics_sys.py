edb8df2620e5291d76c44c85bf4a9934041aac38
Natalia Khaidanova <n.khaidanova@student.vu.nl>
2023-06-13 17:34:25 +0200

    Update traditional_metrics_sys.py

--- traditional_metrics_sys.py
+++ traditional_metrics_sys.py
@@ -1,25 +1,28 @@
 import pandas as pd
 import os
-from sacrebleu.metrics import BLEU, TER, CHRF
+from torchmetrics import SacreBLEUScore, TranslationEditRate, CHRFScore
 
 
 news_data = pd.read_csv('Data/all_news_data.tsv', sep='\t')
-news_candidates = 'WMT21-data/system-outputs/newstest2021/en-ru'
+news_candidates = 'Data/newstest2021/newstest2021_candidates'
 news_references_A = list(news_data['news_ref_A'])
 news_references_B = list(news_data['news_ref_B'])
 
-all_news_references = [news_references_A, news_references_B]
+all_news_references = []
+for A, B in zip(news_references_A, news_references_B):
+    all_news_references.append([A, B])
 
 ted_data = pd.read_csv('Data/all_TED_data.tsv', sep='\t')
-ted_candidates = 'WMT21-data/system-outputs/tedtalks/en-ru'
+ted_candidates = 'Data/tedtalks/tedtalks_candidates'
 ted_references = list(ted_data['TED_ref'])
 
-bleu = BLEU(tokenize = 'char')
-ter = TER()
-chrf2 = CHRF()
+bleu = SacreBLEUScore()
+ter = TranslationEditRate()
+chrf2 = CHRFScore()
+
 
 for domain in ['newstest2021', 'tedtalks']:
-    for metric in ['sacre_BLEU', 'sacre_CHRF2', 'sacre_TER']:
+    for metric in ['sacre_BLEU', 'TER', 'CHRF2']:
         if domain == 'newstest2021':
             
             news_data_dict = {}
@@ -31,16 +34,16 @@ for domain in ['newstest2021', 'tedtalks']:
                     candidates = [str(x) for x in candidates]
                     
                     if metric == 'sacre_BLEU':
-                        sacre_bleu = bleu.corpus_score(candidates, all_news_references)
-                        news_data_dict[file_name[23:-3]] = sacre_bleu.score
+                        sacre_bleu_score = bleu(candidates, all_news_references)
+                        news_data_dict[file_name[23:-3]] = sacre_bleu_score.item()
+                        
+                    if metric == 'TER':
+                        ter_score = ter(candidates, all_news_references)
+                        news_data_dict[file_name[23:-3]] = ter_score.item()
 
-                    if metric == 'sacre_CHRF2':
-                        sacre_chrf2 = chrf2.corpus_score(candidates, all_news_references)
-                        news_data_dict[file_name[23:-3]] = sacre_chrf2.score
-
-                    if metric == 'sacre_TER':
-                        sacre_ter = ter.corpus_score(candidates, all_news_references)
-                        news_data_dict[file_name[23:-3]] = sacre_ter.score
+                    if metric == 'CHRF2':
+                        chrf2_score = chrf2(candidates, all_news_references)
+                        news_data_dict[file_name[23:-3]] = chrf2_score.item()
 
             news_data = pd.DataFrame(news_data_dict, index=[0])
             news_data.to_csv(f'Data/newstest2021/sys/sys_{metric}.tsv', sep='\t', index=False)
@@ -55,18 +58,18 @@ for domain in ['newstest2021', 'tedtalks']:
                 if file_name[19:-3] not in ['ref-A']:
                     ted_data = pd.read_csv('Data/all_TED_data.tsv', sep='\t')
                     candidates = list(ted_data[file_name[19:-3]])
-
+                  
                     if metric == 'sacre_BLEU':
-                        sacre_bleu = bleu.corpus_score(candidates, sacre_references)
-                        ted_data_dict[file_name[19:-3]] = sacre_bleu.score
+                        sacre_bleu_score = bleu(candidates, sacre_references)
+                        ted_data_dict[file_name[19:-3]] = sacre_bleu_score.item()
+                        
+                    if metric == 'TER':
+                        ter_score = ter(candidates, sacre_references)
+                        ted_data_dict[file_name[19:-3]] = ter_score.item()
 
                     if metric == 'sacre_CHRF2':
-                        sacre_chrf2 = chrf2.corpus_score(candidates, sacre_references)
-                        ted_data_dict[file_name[19:-3]] = sacre_chrf2.score
-
-                    if metric == 'sacre_TER':
-                        sacre_ter = ter.corpus_score(candidates, sacre_references)
-                        ted_data_dict[file_name[19:-3]] = sacre_ter.score
+                        chrf2_score = chrf2(candidates, sacre_references)
+                        ted_data_dict[file_name[19:-3]] = chrf2_score.item()
 
             ted_data = pd.DataFrame(ted_data_dict, index=[0])
             ted_data.to_csv(f'Data/tedtalks/sys/sys_{metric}.tsv', sep='\t', index=False)
