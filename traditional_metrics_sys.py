5c2c4fe282045a315fd3333771f47513652a8834
Natalia Khaidanova <n.khaidanova@student.vu.nl>
2023-06-13 13:27:09 +0200

    Update traditional_metrics_sys.py

--- traditional_metrics_sys.py
+++ traditional_metrics_sys.py
@@ -1,45 +1,45 @@
 import pandas as pd
 import os
-import sacrebleu
+from sacrebleu.metrics import BLEU, TER, CHRF
 
 
-news_data = pd.read_csv('all_news_data.tsv', sep='\t')
+news_data = pd.read_csv('Data/all_news_data.tsv', sep='\t')
 news_candidates = 'WMT21-data/system-outputs/newstest2021/en-ru'
 news_references_A = list(news_data['news_ref_A'])
 news_references_B = list(news_data['news_ref_B'])
 
-all_news_references = []
-for A, B in zip(news_references_A, news_references_B):
-    all_news_references.append([A.split(), B.split()])
+all_news_references = [news_references_A, news_references_B]
 
-ted_data = pd.read_csv('all_TED_data.tsv', sep='\t')
+ted_data = pd.read_csv('Data/all_TED_data.tsv', sep='\t')
 ted_candidates = 'WMT21-data/system-outputs/tedtalks/en-ru'
 ted_references = list(ted_data['TED_ref'])
 
+bleu = BLEU()
+ter = TER()
+chrf2 = CHRF()
 
 for domain in ['newstest2021', 'tedtalks']:
     for metric in ['sacre_BLEU', 'sacre_CHRF2', 'sacre_TER']:
         if domain == 'newstest2021':
+            
             news_data_dict = {}
-            sacre_references = []
-            for references in all_news_references:
-                sacre_references.append([' '.join(x) for x in references])
 
             for file_name in os.listdir(news_candidates):
                 if file_name[23:-3] not in ['ref-A', 'ref-B', '']:
-                    news_data = pd.read_csv('all_news_data.tsv', sep='\t')
+                    news_data = pd.read_csv('Data/all_news_data.tsv', sep='\t')
                     candidates = list(news_data[file_name[23:-3]])
-
+                    candidates = [str(x) for x in candidates]
+                    
                     if metric == 'sacre_BLEU':
-                        sacre_bleu = sacrebleu.corpus_bleu(candidates, sacre_references)
+                        sacre_bleu = bleu.corpus_score(candidates, all_news_references)
                         news_data_dict[file_name[23:-3]] = sacre_bleu.score
 
                     if metric == 'sacre_CHRF2':
-                        sacre_chrf2 = sacrebleu.corpus_chrf(candidates, sacre_references)
+                        sacre_chrf2 = chrf2.corpus_score(candidates, all_news_references)
                         news_data_dict[file_name[23:-3]] = sacre_chrf2.score
 
                     if metric == 'sacre_TER':
-                        sacre_ter = sacrebleu.corpus_ter(candidates, sacre_references)
+                        sacre_ter = ter.corpus_score(candidates, all_news_references)
                         news_data_dict[file_name[23:-3]] = sacre_ter.score
 
             news_data = pd.DataFrame(news_data_dict, index=[0])
@@ -53,19 +53,19 @@ for domain in ['newstest2021', 'tedtalks']:
 
             for file_name in os.listdir(ted_candidates):
                 if file_name[19:-3] not in ['ref-A']:
-                    ted_data = pd.read_csv('all_TED_data.tsv', sep='\t')
+                    ted_data = pd.read_csv('Data/all_TED_data.tsv', sep='\t')
                     candidates = list(ted_data[file_name[19:-3]])
 
                     if metric == 'sacre_BLEU':
-                        sacre_bleu = sacrebleu.corpus_bleu(candidates, sacre_references)
+                        sacre_bleu = bleu.corpus_score(candidates, sacre_references)
                         ted_data_dict[file_name[19:-3]] = sacre_bleu.score
 
                     if metric == 'sacre_CHRF2':
-                        sacre_chrf2 = sacrebleu.corpus_chrf(candidates, sacre_references)
+                        sacre_chrf2 = chrf2.corpus_score(candidates, sacre_references)
                         ted_data_dict[file_name[19:-3]] = sacre_chrf2.score
 
                     if metric == 'sacre_TER':
-                        sacre_ter = sacrebleu.corpus_ter(candidates, sacre_references)
+                        sacre_ter = ter.corpus_score(candidates, sacre_references)
                         ted_data_dict[file_name[19:-3]] = sacre_ter.score
 
             ted_data = pd.DataFrame(ted_data_dict, index=[0])
