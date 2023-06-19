7d4133210285d23167e4e43632d4a78c519a5e59
Natalia Khaidanova <n.khaidanova@student.vu.nl>
2023-06-19 12:46:01 +0200

    Update create_data_files.py

--- create_data_files.py
+++ create_data_files.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+
 # GET SOURCE, REFERENCE(S), MACHINE-TRANSLATION OUTPUTS PER DOMAIN
 def read_file(file_path):
     """
@@ -14,14 +15,14 @@ def read_file(file_path):
     return data
   
   
-news_source = r'WMT21-data/sources/newstest2021.en-ru.src.en'
-news_reference_A = r'WMT21-data/references/newstest2021.en-ru.ref.ref-A.ru'
-news_reference_B = r'WMT21-data/references/newstest2021.en-ru.ref.ref-B.ru'
-news_candidates = r'WMT21-data/system-outputs/newstest2021/en-ru'
+news_source = r'Data/WMT21-data/sources/newstest2021.en-ru.src.en'
+news_reference_A = r'Data/WMT21-data/references/newstest2021.en-ru.ref.ref-A.ru'
+news_reference_B = r'Data/WMT21-data/references/newstest2021.en-ru.ref.ref-B.ru'
+news_candidates = r'Data/WMT21-data/system-outputs/newstest2021/en-ru'
 
-ted_source = r'WMT21-data/sources/tedtalks.en-ru.src.en'
-ted_reference = r'WMT21-data/references/tedtalks.en-ru.ref.ref-A.ru'
-ted_candidates = r'WMT21-data/system-outputs/tedtalks/en-ru'
+ted_source = r'Data/WMT21-data/sources/tedtalks.en-ru.src.en'
+ted_reference = r'Data/WMT21-data/references/tedtalks.en-ru.ref.ref-A.ru'
+ted_candidates = r'Data/WMT21-data/system-outputs/tedtalks/en-ru'
 
 
 news_source = read_file(news_source)
@@ -56,140 +57,24 @@ for news_file_name in os.listdir(news_candidates):
 
 
 news_df = pd.DataFrame(news_data_dict)
-news_df.to_csv('all_news_data.tsv', sep='\t', index=False) 
+news_df.to_csv('Data/all_news_data.tsv', sep='\t', index=False) 
 
 ted_df = pd.DataFrame(ted_data_dict)
-ted_df.to_csv('all_TED_data.tsv', sep='\t', index=False) 
+ted_df.to_csv('Data/all_TED_data.tsv', sep='\t', index=False)
 
 
 # GET HUMAN JUDGMENTS FOR EACH TEAM PER DOMAIN, CORRELATION AND METRIC TYPE
+newstest2021_file_path_1 = r'Data/WMT21-data/evaluation/newstest2021/en-ru.mqm.seg.score'
+newstest2021_file_path_2 = r'Data/WMT21-data/evaluation/newstest2021/en-ru.wmt-raw.seg.score'
+newstest2021_file_path_3 = r'Data/WMT21-data/evaluation/newstest2021/en-ru.wmt-z.seg.score'
 
-#file_path_1 = r'WMT21-data/validation/ref-metric.seg.score'
-#file_path_2 = r'WMT21-data/validation/ref-metric.sys.score'
-#file_path_3 = r'WMT21-data/validation/src-metric.seg.score'
-#file_path_4 = r'WMT21-data/validation/src-metric.sys.score'
+newstest2021_file_path_4 = r'Data/WMT21-data/evaluation/newstest2021/en-ru.mqm.sys.score'
+newstest2021_file_path_5 = r'Data/WMT21-data/evaluation/newstest2021/en-ru.wmt-raw.sys.score'
+newstest2021_file_path_6 = r'Data/WMT21-data/evaluation/newstest2021/en-ru.wmt-z.sys.score'
 
+tedtalks_file_path_1 = r'Data/WMT21-data/evaluation/tedtalks/en-ru.mqm.seg.score'
 
-#def get_scores(metric_type, langs, domains, refs, systems, scores):
-    #"""
-    #Put human judgment scores for each system in a dict.
-    
-    #:param string metric_type: if reference-based, metric_type == 'ref'; if reference-free, metric_type == 'src'
-    #:param langs: list of language pairs
-    #:param domains: list of domains
-    #:param refs: list of reference types
-    #:param systems: list of machine-translation systems) 
-    #:param scores: list of human judgment scores
-    #:return: two dicts: for newstest2021 and for tedtalks
-    #"""
-    #news_data_dict, ted_data_dict = {}, {}
-
-    #for lang, domain, ref, system, score in zip(langs, domains, refs, systems, scores):
-        
-        #if metric_type == 'ref':
-            #if lang == 'en-ru' and domain == 'newstest2021' and ref == 'ref-A' and system != 'ref-B':
-                #if system not in news_data_dict:
-                    #news_data_dict[team] = []
-                #news_data_dict[team].append(score)
-                
-        #if metric_type == 'src':
-            #if lang == 'en-ru' and domain == 'newstest2021' and system not in ['ref-A', 'ref-B']:
-                #if system not in news_data_dict:
-                    #news_data_dict[team] = []
-                #news_data_dict[team].append(score)
-
-        #if lang == 'en-ru' and domain == 'tedtalks':
-            #if system not in ted_data_dict:
-                #ted_data_dict[team] = []
-            #ted_data_dict[team].append(score)
-            
-    #return news_data_dict, ted_data_dict
-
-
-#def save_scores(file_path, correlation, metric_type):
-    #"""
-    #Save all scores per system in a .tsv file. 
-    
-    #:param string file_path: path to the validation file
-    #:param string correlation: if segment-level, correlation == 'seg'; if system-level, correlation == 'sys'
-    #:param string metric_type: if reference-based, metric_type == 'ref'; if reference-free, metric_type == 'src'
-    #:return: None
-    #"""
-
-    #if correlation == 'seg':
-
-        #labels = ['file','lang','domain','ref','system','src_index','score']
-        #data = pd.read_csv(file_path, sep='\t', on_bad_lines='skip', keep_default_na=False, names=labels) 
-
-        #langs = list(data['lang'])
-        #domains = list(data['domain'])
-        #refs = list(data['ref'])
-        #systems = list(data['system'])
-        #scores = list(data['score'])
-
-        #if metric_type == 'ref':
-
-            #news_data_dict, ted_data_dict = get_scores('ref', langs, domains, refs, systems, scores)
-
-            #news_df = pd.DataFrame(news_data_dict)
-            #news_df.to_csv('all_news_seg_ref_scores.tsv', sep='\t', index=False) 
-
-            #ted_df = pd.DataFrame(ted_data_dict)
-            #ted_df.to_csv('all_TED_seg_ref_scores.tsv', sep='\t', index=False) 
-
-        #if metric_type == 'src':
-
-            #news_data_dict, ted_data_dict = get_scores('src', langs, domains, refs, systems, scores)
-
-            #news_df = pd.DataFrame(news_data_dict)
-            #news_df.to_csv('all_news_seg_src_scores.tsv', sep='\t', index=False) 
-
-            #ted_df = pd.DataFrame(ted_data_dict)
-            #ted_df.to_csv('all_TED_seg_src_scores.tsv', sep='\t', index=False) 
-
-    #if correlation == 'sys':
-
-        #labels = ['file','lang','domain','ref','system','score']
-        #data = pd.read_csv(file_path, sep='\t', on_bad_lines='skip', keep_default_na=False, names=labels) 
-
-        #langs = list(data['lang'])
-        #domains = list(data['domain'])
-        #refs = list(data['ref'])
-        #systems = list(data['system'])
-        #scores = list(data['score'])
-
-        #if metric_type == 'ref':
-
-            #news_data_dict, ted_data_dict = get_scores('ref', langs, domains, refs, systems, scores)
-
-            #news_df = pd.DataFrame(news_data_dict)
-            #news_df.to_csv('all_news_sys_ref_scores.tsv', sep='\t', index=False) 
-
-            #ted_df = pd.DataFrame(ted_data_dict)
-            #ted_df.to_csv('all_TED_sys_ref_scores.tsv', sep='\t', index=False) 
-            
-        #if metric_type == 'src':
-            
-            #news_data_dict, ted_data_dict = get_scores('src', langs, domains, refs, systems, scores)
-
-            #news_df = pd.DataFrame(news_data_dict)
-            #news_df.to_csv('all_news_sys_src_scores.tsv', sep='\t', index=False) 
-
-            #ted_df = pd.DataFrame(ted_data_dict)
-            #ted_df.to_csv('all_TED_sys_src_scores.tsv', sep='\t', index=False) 
-            
-            
-#if __name__ == '__main__':
-    #save_scores(file_path_1, 'seg', 'ref')
-    #save_scores(file_path_2, 'sys', 'ref')
-    #save_scores(file_path_3, 'seg', 'src')
-    #save_scores(file_path_4, 'sys', 'src')
-    
-    
-file_path_1 = r'mt-metrics-eval-v2/wmt21.news/human-scores/en-ru.mqm.seg.score'
-file_path_2 = r'mt-metrics-eval-v2/wmt21.tedtalks/human-scores/en-ru.mqm.seg.score'
-file_path_3 = r'mt-metrics-eval-v2/wmt21.news/human-scores/en-ru.mqm.sys.score'
-file_path_4 = r'mt-metrics-eval-v2/wmt21.tedtalks/human-scores/en-ru.mqm.sys.score'
+tedtalks_file_path_2 = r'Data/WMT21-data/evaluation/tedtalks/en-ru.mqm.sys.score'
 
 
 def get_scores(file_path, systems, scores):
@@ -219,12 +104,13 @@ def get_scores(file_path, systems, scores):
     return news_data_dict, ted_data_dict
 
 
-def save_scores(file_path, correlation):
+def save_scores(file_path, correlation, score_type):
     """
     Save all scores per system in a .tsv file. 
     
     :param string file_path: path to the validation file
     :param string correlation: if segment-level, correlation == 'seg'; if system-level, correlation == 'sys'
+    :param string score_type: type of human judgment scores ('mqm', 'raw_da', or 'z_da')
     :return: None
     """
 
@@ -240,11 +126,11 @@ def save_scores(file_path, correlation):
         
         if 'news' in file_path:
             news_df = pd.DataFrame(news_data_dict)
-            news_df.to_csv('all_news_seg_mqm_scores.tsv', sep='\t', index=False) 
+            news_df.to_csv(f'eval/human_judgments_seg/all_news_seg_{score_type}_scores.tsv', sep='\t', index=False) 
             
         if 'tedtalks' in file_path:
             ted_df = pd.DataFrame(ted_data_dict)
-            ted_df.to_csv('all_TED_seg_mqm_scores.tsv', sep='\t', index=False) 
+            ted_df.to_csv('eval/human_judgments_seg/all_TED_seg_mqm_scores.tsv', sep='\t', index=False) 
 
     if correlation == 'sys':
 
@@ -258,14 +144,20 @@ def save_scores(file_path, correlation):
         
         if 'news' in file_path:
             news_df = pd.DataFrame(news_data_dict)
-            news_df.to_csv('all_news_sys_mqm_scores.tsv', sep='\t', index=False) 
+            news_df.to_csv(f'eval/human_judgments_sys/all_news_sys_{score_type}_scores.tsv', sep='\t', index=False) 
         if 'tedtalks'in file_path:
             ted_df = pd.DataFrame(ted_data_dict)
-            ted_df.to_csv('all_TED_sys_mqm_scores.tsv', sep='\t', index=False) 
+            ted_df.to_csv('eval/human_judgments_sys/all_TED_sys_mqm_scores.tsv', sep='\t', index=False) 
             
             
 if __name__ == '__main__':
-    save_scores(file_path_1, 'seg')
-    save_scores(file_path_2, 'seg')
-    save_scores(file_path_3, 'sys')
-    save_scores(file_path_4, 'sys')
+    save_scores(newstest2021_file_path_1, 'seg', 'mqm')
+    save_scores(newstest2021_file_path_2, 'seg', 'raw_da')
+    save_scores(newstest2021_file_path_3, 'seg', 'z_da')
+    
+    save_scores(newstest2021_file_path_4, 'sys', 'mqm')
+    save_scores(newstest2021_file_path_5, 'sys', 'raw_da')
+    save_scores(newstest2021_file_path_6, 'sys', 'z_da')
+    
+    save_scores(tedtalks_file_path_1, 'seg', 'mqm')
+    save_scores(tedtalks_file_path_2, 'sys', 'mqm')
