9ac5a4a0d798670cd5fa38d861adb9daa51d7140
Natalia Khaidanova <n.khaidanova@student.vu.nl>
2023-06-19 16:00:42 +0200

    Update add_opus_mt_translations.py

--- reference-free/add_opus_mt_translations.py
+++ reference-free/add_opus_mt_translations.py
@@ -8,7 +8,7 @@ model = MarianMTModel.from_pretrained(model_name)
 
 for file in ['baby_k.tsv', 'a_beautiful_mind.tsv']:
 
-    df = pd.read_csv(file, sep='\t')
+    df = pd.read_csv(f'Data/{file}', sep='\t')
     source = list(df['source'])
     
     translated = model.generate(**tokenizer(source, return_tensors="pt", padding=True))
@@ -18,4 +18,4 @@ for file in ['baby_k.tsv', 'a_beautiful_mind.tsv']:
         opus_mt_translations.append(tokenizer.decode(sentence, skip_special_tokens=True))
         
     df['opus_mt_translation'] = opus_mt_translations
-    df.to_csv(file, sep='\t', index=False)
+    df.to_csv(f'Data/{file}', sep='\t', index=False)
